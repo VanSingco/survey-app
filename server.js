6bd32470955225b90fa0dfaac1f83ab247845355
Van Zachary Singco19 <edwincarneses@gmail.com>
2018-07-25 15:49:17 +0800

    hello fullstact react and node

--- server.js
+++ server.js
@@ -3,17 +3,21 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
-const {database} = require('./config/config')
+const {database} = require('./server/config/secret')
 
 const app = express();
 
 // connect to mongodb database
-mongoose.Promise = global.Promise;
-mongoose.connect(database);
+// mongoose.Promise = global.Promise;
+// mongoose.connect(database);
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.send('Hello FullStact React And NodeJS')
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT , () => {
